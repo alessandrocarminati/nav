9b67d6dd0f274e156a093409212a78cc50e67797
Maurizio Papini <maurizio.papini@gmail.com>
2022-08-02 11:02:24 +0200

    Use cache for node and a map to track visited nodes

--- nav.go
+++ nav.go
@@ -1,21 +1,12 @@
 package main
+
 //import (
 //	"fmt"
 //	)
 
-
 func main() {
-	t:=Connect_token{"dbs.hqhome163.com",5432,"alessandro","<password>","kernel"}
-	db:=Connect_db(&t)
-/*é
-	succ, err :=get_successors_by_id(db, 153735)
-	if err!= nil {
-		panic(err)
-		}
-	for _, e := range succ {
-		fmt.Println(e, ",,,,,,,,,,,;;")
-		}
-*/
-	Navigate(db, 153735, nil)
-}
+	t := Connect_token{"localhost", 15432, "test", "test", "kernel"}
+	db := Connect_db(&t)
 
+	Navigate(db, 153735, make(map[int]bool), make(map[int]Entry))
+}
